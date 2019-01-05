a95f17f7a239d68bb2994d42721d40c80fc842de
luis <hamburger120@outlook.com>
2019-01-05 10:55:33 -0500

    3

--- assets/javascript/weatherapi.js
+++ assets/javascript/weatherapi.js
@@ -4,33 +4,45 @@ $(document).ready(function(){
    function apiCall(zip,country){
     // var zip = zip;
     // var country= country; 
-    var url= "http://api.openweathermap.org/data/2.5/weather?zip="+zip+","+country+"&units=imperial&appid=ee0fc7e4084dd72152033db8d2dac3d0";
+    var url= "https://api.openweathermap.org/data/2.5/weather?zip="+zip+","+country+"&units=imperial&appid=ee0fc7e4084dd72152033db8d2dac3d0";
     // console.log(url);
         $.ajax({
         url: url,
         method: "GET"
     }).then(function(response){
+       
+       console.log("-----------------")
         console.log(response);
+          
+        // showing info on the html
+          if ($(".weather-img").click) {
+            $(".news-card").hide();
+            $(".social-card").hide();
+            $("#weather").show();
+            $("#city").text(response.name);
+            $("#country").text(response.sys.country); 
+            $("#max").text(response.main.temp_max); 
+            $("#min").text(response.main.temp_min);   
+            $("#humidity").text(response.main.humidity);
+            $("#description").text(response.weather[0].description);    
+        }
+        else if($(".news-img").click){
+            $("#weather").hide();
+            $(".news-card").show();
+            $(".social-card").hide();
+        }
+
     });
 };
-    // initializing database
-    var config = {
-        apiKey: "AIzaSyDZk2G8b4OOiN3rW0wDALk0ITxY2UdL0PY",
-        authDomain: "teamwinnersproject1.firebaseapp.com",
-        databaseURL: "https://teamwinnersproject1.firebaseio.com",
-        projectId: "teamwinnersproject1",
-        storageBucket: "teamwinnersproject1.appspot.com",
-        messagingSenderId: "991653674785"
-      };
-      firebase.initializeApp(config);
-      var database = firebase.database();
+ 
+    var database = firebase.database();
     //  event for when a new object is added to the database
     
     
       $(".weather-img").on("click", function(){
         
         database.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
-        // Change the HTML to reflect
+        // putting info in variables
         var city = snapshot.val().destCity;
         var country = snapshot.val().destCountry;
         var zip = snapshot.val().destZip;
